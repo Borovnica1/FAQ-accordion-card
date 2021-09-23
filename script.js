92693a6d59d1547efc5cecfef3e2162ad8fd5ddc
Borovnica1 <petkovic.czv1@gmail.com>
2021-09-23 17:35:50 +0200

    debug questionsOpened properly tracks which questions are opened

--- script.js
+++ script.js
@@ -19,13 +19,17 @@ for (let question of questions) {
       arrow.style.transform = 'rotate(0deg)';
       questionTitle.style.fontWeight = '400';
       questionsOpenedCnt--;
+
+      // find index of the question we are closing in questionOpened
+      let questionOpenedIndex = questionsOpened.findIndex(el => el == answerIndex);
+      questionsOpened.splice(questionOpenedIndex, 1);
     } else {
       answerParagraph.classList.add('opened');
       arrow.style.transform = 'rotate(180deg)'
       questionTitle.style.fontWeight = '700';
       questionsOpenedCnt++;
       questionsOpened.push(answerIndex);
-
+      console.log('LOL', questionsOpenedCnt, questionsOpened);
       // if more than 3 questions opened, close the first one opened because there is not enough space to open all answers
       if (questionsOpenedCnt > 3) {
         firstAnswerOpenedIndex = questionsOpened.shift();
